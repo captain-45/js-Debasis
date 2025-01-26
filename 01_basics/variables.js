462e50bc154515b14123f190fbd4478d71c7e632
Debasis panda <captainbullakad@gmail.com>
2025-01-26 17:25:56 +0000

    dataTypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const account_Id = 144553
+let account_Email = "pandadebsis510@gmail.com"
+var account_Password = "55544"
+account_City = "kendrapara"
+
+account_Email = "ddgg"
+account_Password = "fgd"
+account_City = "dr5"
+//account_Id = 2
+console.log(account_Id);
+console.table([account_Id ,account_Email, account_Password, account_Password, account_City])
