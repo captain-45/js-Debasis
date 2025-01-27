bc9f3bf5517a429b975a5996afaac2afdec98824
Debasis panda <captainbullakad@gmail.com>
2025-01-27 04:24:56 +0000

    operation

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -28,9 +28,31 @@ console.log(booleanIsLoggedIn)
 let someNumber = 45
 
 let stringSomeNumber = String(someNumber)
-
 console.log(stringSomeNumber)
 console.log(typeof stringSomeNumber)
 /**
  * here type converted to string
  */
+
+//*************************operation***********************
+ 
+let value = 6
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello "
+let str2 = "Debasis"
+str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 5)
+console.log(1 + "5")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "5")
+
+/**
+ * In javaScript if String  comes first in opeation case then all next values are treated as String(case 1 and 3) but if number comes first and then after a String value is placed then the fisrt number values are added with each other and then concatinated with the lastly located String value(caase 4).
+ */
+
+ 
+
