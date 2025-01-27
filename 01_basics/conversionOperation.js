3ee917b9b8f69a8c00cf2a4170cf93bb6c2b4017
Debasis panda <captainbullakad@gmail.com>
2025-01-27 03:50:38 +0000

    datatypes and conversion operation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "bcg45"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; 
+false => 0
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/**
+ * 1 => true
+ * 0 => false
+ * "" => false
+ * "Debasis" => true
+ */
+
+let someNumber = 45
+
+let stringSomeNumber = String(someNumber)
+
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
+/**
+ * here type converted to string
+ */
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //in node js not possible but posible in browser console
+
+console.log(3 + 3);
+
+let name = "Debasis"
+let age = 0
+let isLoggedIn = false
+
+// primitive data types
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => trye/false
+null => standalone value
+undefined => 
+symbol => unique
+
+
+
+object 
+
+
+*/
+
+console.log(typeof name)
