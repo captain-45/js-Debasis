e69f6af6c2427233df1edfe12f3250f949d771b9
Debasis panda <captainbullakad@gmail.com>
2025-01-27 03:46:02 +0000

    conversion data types

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -6,6 +6,10 @@ account_City = "kendrapara"
 account_Email = "ddgg"
 account_Password = "fgd"
 account_City = "dr5"
-//account_Id = 2
+//account_Id = 2 //cannot assign another value to a constant value
 console.log(account_Id);
-console.table([account_Id ,account_Email, account_Password, account_Password, account_City])
+console.table([account_Id, account_Email, account_Password, account_Password, account_City])
+/*
+prefer to not use var 
+because of issue in block scope and functional scope  
+*/
